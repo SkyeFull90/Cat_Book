0ef5ef917dc6bbd3a7c613d6277ae16f6bc1f47c
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-09 13:20:56 -0700

    Fix deleteCat function and add new images

--- controllers/catController.js
+++ controllers/catController.js
@@ -95,7 +95,13 @@ const deleteCat = async (req, res) => {
   try {
     let cat = await Cat.findById(req.params.id);
     if (cat.owner.equals(req.user._id)) {
-      await Cat.findByIdAndRemove(req.params.id);
+      await Cat.findByIdAndDelete(req.params.id, function(err){
+        if(err){
+          console.log(err);
+        } else {
+          console.log("deleted");
+        }
+      });
     }
     res.redirect('/');
   } catch(err) {
