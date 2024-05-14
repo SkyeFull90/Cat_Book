5a615c2ffd396cac50b469e11a63c4c9d548013b
skye <full.skyl@hotmail.com>
2024-05-14 16:12:39 -0600

    removing a uncessary commented line

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ connectDB()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
